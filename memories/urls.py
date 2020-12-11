f390151d76f3010e4fd3375ab1696ab1641a343c
jellyfish12jellyfish <grinvichforum12@mail.ru>
2020-12-11 15:42:41 +0500

    Add new urls

--- memories/urls.py
+++ memories/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
-from .views import *
+from . import views
 
 app_name = 'memories'
 urlpatterns = [
-    path('', home, name='home'),
+    path('', views.home, name='home'),
+    path('login/', views.login, name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
