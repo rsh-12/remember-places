ae9d4b0eb5cc203d273e52dc979479bba1936952
jellyfish12jellyfish <grinvichforum12@mail.ru>
2020-12-11 14:06:18 +0500

    Move key to secret_vars file

--- places_remember/settings.py
+++ places_remember/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import secret_vars
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'd3ofb_=ddr_hwd2i54a*@_+&8^l-2e%oekn7x*ot0x1q7tw#3d'
+SECRET_KEY = secret_vars.KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
