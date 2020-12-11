8cb6dd4402717026c422e999c835b793eaea93b7
jellyfish12jellyfish <grinvichforum12@mail.ru>
2020-12-11 15:34:28 +0500

    Add get controllers for pages

--- memories/views.py
+++ memories/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
 def home(request):
     return render(request, 'memories/home.html')
+
+
+def login(request):
+    return render(request, 'memories/login.html')
+
+
+@login_required
+def profile(request):
+    return render(request, 'memories/profile.html')
