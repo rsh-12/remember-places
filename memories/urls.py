2a89cc8ea62be577775fbb47c11cdb19ba85d99d
jellyfish12jellyfish <grinvichforum12@mail.ru>
2020-12-11 16:07:59 +0500

    Update some files
    Add profile url and put social_django in INSTALLED_APPS

--- memories/urls.py
+++ memories/urls.py
@@ -4,8 +4,10 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'memories'
+
 urlpatterns = [
     path('', views.home, name='home'),
+    path('profile/', views.profile, name='profile'),
     path('login/', views.login, name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
