c970ee9b86b416104da622f45b3639d9094ffdb6
jellyfish12jellyfish <grinvichforum12@mail.ru>
2020-12-12 10:03:41 +0500

    Update Place model, add lat, long fields

--- memories/models.py
+++ memories/models.py
@@ -7,6 +7,10 @@ class Place(models.Model):
     description = models.TextField(blank=False, max_length=500)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    # todo: change to geofield with postgres
+    latitude = models.DecimalField(max_digits=22, decimal_places=16, blank=True, null=True)
+    longitude = models.DecimalField(max_digits=22, decimal_places=16, blank=True, null=True)
+
     users = models.ManyToManyField(User)
 
     class Meta:
