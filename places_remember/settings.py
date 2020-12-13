f771035a3c9e7bb7d1cb6fdf27ed61e60890316b
jellyfish12jellyfish <grinvichforum12@mail.ru>
2020-12-13 21:07:13 +0500

    Add app domain to allowed_hosts and set debug=false

--- places_remember/settings.py
+++ places_remember/settings.py
@@ -54,9 +54,9 @@ LOGGING = {
 # SECRET_KEY = secret_vars.KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', 'places-of-memories.herokuapp.com']
 
 # Application definition
 
