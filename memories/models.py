64e5512fc808a395368c2c448bba9b433fb83a69
jellyfish12jellyfish <grinvichforum12@mail.ru>
2020-12-13 22:00:15 +0500

    Update Place model

--- memories/models.py
+++ memories/models.py
@@ -5,10 +5,9 @@ from django.urls import reverse
 
 class Place(models.Model):
     name = models.CharField(max_length=50, blank=False)
-    description = models.TextField(blank=False, max_length=500)
+    description = models.TextField(blank=False, max_length=600)
     created_at = models.DateTimeField(auto_now_add=True)
 
-    # todo: change to geofield with postgres
     latitude = models.DecimalField(max_digits=22, decimal_places=16, blank=True, null=True)
     longitude = models.DecimalField(max_digits=22, decimal_places=16, blank=True, null=True)
 
