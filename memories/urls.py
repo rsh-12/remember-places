7aaee7964fc6fd4ab36995ab1116ec006321859d
jellyfish12jellyfish <grinvichforum12@mail.ru>
2020-12-14 18:45:37 +0500

    Optimize imports

--- memories/urls.py
+++ memories/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from django.views.generic import TemplateView
 
 from . import views
 from django.views.generic import TemplateView
