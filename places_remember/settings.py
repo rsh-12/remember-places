3ade2e0e31d4bb2b0c9b31b7a0f90a033c36538d
jellyfish12jellyfish <grinvichforum12@mail.ru>
2020-12-14 13:28:28 +0500

    Update settings

--- places_remember/settings.py
+++ places_remember/settings.py
@@ -100,8 +100,8 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
 
-                # 'social_django.context_processors.backends',
-                # 'social_django.context_processors.login_redirect',
+                'social_django.context_processors.backends',
+                'social_django.context_processors.login_redirect',
             ],
         },
     },
