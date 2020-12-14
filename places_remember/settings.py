47e7c8cd816ad58245e10da9404d90e9e704c077
jellyfish12jellyfish <grinvichforum12@mail.ru>
2020-12-14 12:34:43 +0500

    Update settings

--- places_remember/settings.py
+++ places_remember/settings.py
@@ -186,6 +186,9 @@ LOGOUT_REDIRECT_URL = '/'
 # SOCIAL_AUTH_FACEBOOK_KEY = os.environ['FB_KEY']
 # SOCIAL_AUTH_FACEBOOK_SECRET = os.environ['FB_SECRET']
 
+SOCIAL_AUTH_FACEBOOK_KEY = '172001884657832'
+SOCIAL_AUTH_FACEBOOK_SECRET = 'ee8a6f737664c6ca2b8186349e3c9cff'
+
 # SOCIAL_AUTH_FACEBOOK_SCOPE = ['email', 'user_link']
 # TODO вернуть в случае провала аутентификации через ФБ
 # SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
