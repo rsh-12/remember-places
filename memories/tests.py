08e77c6b11dc259964f39dbc09dc1f731dddff9c
jellyfish12jellyfish <grinvichforum12@mail.ru>
2020-12-17 14:44:36 +0500

    Add fixtures and fix tests

--- memories/tests.py
+++ memories/tests.py
@@ -6,46 +6,38 @@ from memories.models import Place
 
 
 class PlaceTestCase(TestCase):
+    fixtures = ['memories/fixtures/users.json', 'memories/fixtures/places.json']
 
-    # create user, place and get username and description
     def test_should_return_user_place(self):
-        user = create_user("Lika")
-        place = create_place("Cafe", "Very nice!", user_id=user.id)
+        user = User.objects.get(pk=2)
+        place = Place.objects.get(pk=9)
+        self.assertEqual(place.name, 'University')
 
-        self.assertEqual("Lika", user.username)
-        self.assertEqual("Very nice!", place.description)
+        self.assertEqual(user.username, 'test')
+        self.assertTrue(place.description.__contains__('Lorem ipsum dolor'))
 
-    # create 2 users and 4 places.
-    # check request: find places only for user with id 1
-    def test_should_return_3(self):
-        # user and his places
-        user = create_user("Lika")
-        create_place("Place1", "Beautiful park1", user.id)
-        create_place("Place2", "Beautiful park2", user.id)
-        create_place("Place3", "Beautiful park4", user.id)
-
-        # user2 and his places
-        user2 = create_user("Another username")
-
-        create_place("Place4", "It shouldn't count as a place for user with id 1", user2.id)
-
+    def test_should_return_0(self):
+        user = User.objects.get(pk=3)
         places = user.place_set.all()
         response = self.client.get(reverse('memories:memories'))
 
         self.assertNotEqual(404, response.status_code)
-        self.assertEqual(3, places.count())
+        self.assertEqual(0, places.count())
 
-    # test home url
+    def test_should_return_3(self):
+        user = User.objects.get(pk=2)
+        places = user.place_set.count()
+
+        self.assertEqual(places, 3)
+
+    def test_should_return_ok(self):
+        user = User.objects.get(pk=3)
+        Place.objects.create(name="New place", description="New description", user_id=user.id)
+        Place.objects.create(name="New place2", description="New description2", user_id=user.id)
+
+        self.assertEqual(user.place_set.count(), 2)
+        self.assertTrue(user.place_set.get(pk=12).name, "New description")
+
     def test_home_url(self):
         response = self.client.get(reverse('memories:home'))
         self.assertEqual(200, response.status_code)
-
-
-def create_user(name):
-    user = User.objects.create(username=name)
-    return user
-
-
-def create_place(place_name, description, user_id):
-    place = Place.objects.create(name=place_name, description=description, user_id=user_id)
-    return place
