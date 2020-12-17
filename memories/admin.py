c5b6fdee908c2bf51a555c90f561d38da585dcd7
jellyfish12jellyfish <grinvichforum12@mail.ru>
2020-12-17 13:32:36 +0500

    Add user to the list_display

--- memories/admin.py
+++ memories/admin.py
@@ -5,5 +5,5 @@ from .models import Place
 
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
-    list_display = ['name', 'created_at']
+    list_display = ['name', 'created_at', 'user']
     list_filter = ['name', 'created_at']
