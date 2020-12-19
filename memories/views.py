0b0509dd049b62c0194a4f414d43d27e8d255272
jellyfish12jellyfish <grinvichforum12@mail.ru>
2020-12-19 07:19:30 +0500

    Optimize imports

--- memories/views.py
+++ memories/views.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.urls import reverse_lazy
