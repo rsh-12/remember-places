0b79de3cdfddd0d7a754115c6d4b755b0e85e5b2
jellyfish12jellyfish <grinvichforum12@mail.ru>
2020-12-22 21:17:57 +0500

    Add message on the profile page, add form_valid function

--- user/views.py
+++ user/views.py
@@ -1,17 +1,17 @@
+from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm
-
-# user registration
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 from django.contrib.auth.views import PasswordChangeView
-from django.shortcuts import redirect
+from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, UpdateView
 
 from user.forms import UserUpdateModelForm
 
 
+# user registration
 class UserRegistrationView(CreateView):
     template_name = 'registration/registration.html'
     form_class = UserCreationForm
@@ -25,11 +25,19 @@ class UserRegistrationView(CreateView):
         return result
 
 
+# user update profile (firstname, lastname, email)
 class UserUpdateView(LoginRequiredMixin, UpdateView):
     model = User
     form_class = UserUpdateModelForm
     template_name = "user/profile.html"
-    success_url = "/profile/update/{id}/"
+
+    def form_valid(self, form):
+        if form.is_valid():
+            messages.success(self.request, 'Profile updated successfully!')
+            form.save()
+            return render(self.request, 'user/profile.html', {'form': form})
+        messages.warning(self.request, 'Something went wrong! Please try again!')
+        return render(self.request, 'user/profile.html', {'form': form})
 
 
 class UserPasswordChangeView(LoginRequiredMixin, PasswordChangeView):
