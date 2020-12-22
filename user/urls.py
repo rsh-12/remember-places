de4be86512696396dc23084b16181e04e9462277
jellyfish12jellyfish <grinvichforum12@mail.ru>
2020-12-22 14:06:22 +0500

    Fix login and logout urls

--- user/urls.py
+++ user/urls.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.views import PasswordChangeView, LoginView, PasswordChangeDoneView
+from django.contrib.auth.views import PasswordChangeView, LoginView, PasswordChangeDoneView, LogoutView
 from django.urls import path
 from django.views.generic import TemplateView
 
@@ -8,11 +8,13 @@ app_name = 'user'
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name="user/profile.html"), name='profile'),
-    path('password_change/',
-         PasswordChangeView.as_view(template_name="registration/change_password.html"), name='password_change')
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('registration/', UserRegistrationView.as_view(), name='registration'),
 
-    # path('login/', LoginView.as_view(), name='login'),
-    # path('registration/', UserRegistrationView.as_view(), name='registration'),
+    # path('password_change/',
+    #      PasswordChangeView.as_view(template_name="registration/change_password.html"), name='password_change'),
+
     # path('password_change/done/',
     #      PasswordChangeDoneView.as_view(template_name="registration/password_change_done.html"),
     #      name="password_change_done"),
