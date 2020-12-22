2f95c8a31443ca4c43581ab86cc025528c8082a8
jellyfish12jellyfish <grinvichforum12@mail.ru>
2020-12-22 18:50:33 +0500

    Add user update controller

--- user/views.py
+++ user/views.py
@@ -2,8 +2,10 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm
 
 # user registration
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 
 
 class UserRegistrationView(CreateView):
@@ -17,3 +19,10 @@ class UserRegistrationView(CreateView):
         user = authenticate(username=cd['username'], password=cd['password1'])
         login(self.request, user)
         return result
+
+
+class UserUpdateView(LoginRequiredMixin, UpdateView):
+    model = User
+    fields = ['username', 'first_name', 'last_name', 'email']
+    template_name = "user/profile.html"
+    success_url = reverse_lazy('user:profile')
