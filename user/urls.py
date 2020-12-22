85f817c478b1215e161781d39b8c83db36518e1e
jellyfish12jellyfish <grinvichforum12@mail.ru>
2020-12-22 18:52:15 +0500

    Fix user update urls

--- user/urls.py
+++ user/urls.py
@@ -7,7 +7,7 @@ from user.views import *
 app_name = 'user'
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name="user/profile.html"), name='profile'),
+    # path('', TemplateView.as_view(template_name="user/profile.html"), name='profile'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('registration/', UserRegistrationView.as_view(), name='registration'),
