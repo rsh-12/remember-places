7478db5b8589c7eb4d0fe2ef4dfd34c6057635fa
jellyfish12jellyfish <grinvichforum12@mail.ru>
2020-12-24 20:26:20 +0500

    Set autofocus after click

--- static/js/map.js
+++ static/js/map.js
@@ -4,13 +4,14 @@ var myMap;
 function getCoords(x, y) {
     document.getElementById('id_latitude').value = x
     document.getElementById('id_longitude').value = y
+    document.getElementById('id_name').focus();
 }
 
 function init() {
     myMap = new ymaps.Map("map", {
         center: [56.83507546894007, 60.59762278832995], // Екб
         zoom: 12,
-        controls:['searchControl', 'zoomControl']
+        controls: ['searchControl', 'zoomControl']
     }, {
         balloonMaxWidth: 200,
         searchControlProvider: 'yandex#search'
