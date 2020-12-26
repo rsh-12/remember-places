030b1fdaa309dd6ac95e5bb3c9aaaca986efb91d
jellyfish12jellyfish <grinvichforum12@mail.ru>
2020-12-26 14:53:42 +0500

    Set postgres db

--- places_remember/settings.py
+++ places_remember/settings.py
@@ -94,8 +94,12 @@ WSGI_APPLICATION = 'places_remember.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
+        'HOST': os.environ['DB_HOST'],
+        'PORT': 5432,
     }
 }
 
